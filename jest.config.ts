8ffd2b7d96566fc758e9253033c59c2146104c5c
Mickael Brunet <mickael.brunet64@gmail.com>
2023-11-24 16:59:22 +0100

    pass tests

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+import type { Config } from '@jest/types';
+const config: Config.InitialOptions = {
+    verbose: true,
+    transform: {
+        '^.+\\.ts?$': 'ts-jest'
+    }
+}
+
+export default config;
