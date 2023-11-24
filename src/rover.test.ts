a94a8b466d4f724a8fa738c5ba3fe55be820d502
Mickael Brunet <mickael.brunet64@gmail.com>
2023-11-24 16:30:52 +0100

    commit

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,21 +1,30 @@
 import Rover from "./rover";
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-});  // PASS
+describe('Rover Class Tests', () => {
+  let rover: Rover;
+  const initialPosition = { x: 0, y: 0 };
 
-beforeAll(() => {
+  beforeEach(() => {
+    rover = new Rover(0, initialPosition);
+  });
 
-})
+  it('should initialize with correct orientation and position', () => {
+    expect(rover.orientation).toBe(0);
+    expect(rover.position).toEqual(initialPosition);
+  });
 
-const rover = new Rover(0, { x: 0, y: 0 })
+//   it('should turn to the specified orientation', () => {
+//     rover.turn();
+//     expect(rover.orientation).toBe(2);
 
-test('Rover should go forward', () => {
-    // const rover = new Rover(0, { x: 0, y: 0 })
-    rover.move(1)
+//     rover.turn(Orientation.Est);
+//     expect(rover.orientation).toBe(1);
+//   });
+
+  it('should move forward or backward correctly based on orientation', () => {
+    rover.move(1);
     expect(rover.position).toEqual({ x: 0, y: 1 });
+  });
+
+  // Add more test cases as needed for other scenarios
 });
-
-test('Rover should turn right', () => {
-
-})
