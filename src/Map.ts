b00657dddecbb724880cd66a4fa91860a86d0299
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-12-21 17:01:31 +0100

    :fire: remove useless comment

--- src/Map.ts
+++ src/Map.ts
@@ -17,7 +17,6 @@ export class Map {
     }
 
     hasObstacleAt(x: number, y: number): boolean {
-        // Vérifier si un obstacle se trouve à la position spécifiée
         return this._obstacles.some(obstacle => obstacle.position.x === x && obstacle.position.y === y);
     }
 }
